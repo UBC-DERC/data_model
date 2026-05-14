e3bcae07409bca30bd39b476e3a3aab70e4622c8
Simon <simon.j.goring@gmail.com>
2026-05-14 10:34:36 -0700

    Cleaned up the `main.py` file to load the whole database
    Right now, there's not much else we need it to do, other than to load the whole thing and print it to screen.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 
 def main():
     import data_model as dm
+    import pathlib as pt
     from yaml import safe_load
     
-    schema = safe_load("validation.yaml")
-    cows = dm.load_files("data_definitions/tables/cows.yaml")
-    dm.validate(schema, cows)
-    
-
+    with open(pt.Path("validation.yaml"), 'r') as schemaFile:
+        validation = safe_load(schemaFile)
+    cows = dm.load_database("data_definitions/dairymodel.yaml", validation = validation)
+    print(cows)
 if __name__ == "__main__":
     main()
