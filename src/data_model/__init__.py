63ef2619803302866de51d299b2100b23be2f7a3
Simon <simon.j.goring@gmail.com>
2026-07-02 16:42:12 -0700

    Add argparse CLI (data-model) with verbose failure and working entry point

--- src/data_model/__init__.py
+++ src/data_model/__init__.py
@@ -3,6 +3,7 @@ from .load_database import load_database as load_database
 from .load_schema import load_schema as load_schema
 from .load_tables import load_tables as load_tables
 from .createDocs import document_database as document_database
+from .cli import main as main
 from .model_build import (
     build_tables as build_tables,
     ModelValidationError as ModelValidationError,
